966c834d133c9afc875008a29d7fe1bf53126bcd
gkarthik <xraydiffraction@gmail.com>
2015-02-05 20:40:04 +0530

    Added ChatHead Service. Chat head shown on top of lock screen using TYPE_SYSTEM_OVERLAY.

---
+++ app/src/main/java/com/example/skywalker/rounded_image/CircularImageView.java
@@ -0,0 +1,38 @@
+package com.example.skywalker.rounded_image;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by skywalker on 2/5/15.
+ */
+    public class CircularImageView extends ImageView {
+
+        public CircularImageView(Context context) {
+            super(context);
+        }
+
+        public CircularImageView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public CircularImageView(Context context, AttributeSet attrs, int defStyle) {
+            super(context, attrs, defStyle);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            float radius = 90.0f;
+            Path clipPath = new Path();
+            RectF rect = new RectF(0, 0, this.getWidth(), this.getHeight());
+            clipPath.addRoundRect(rect, radius, radius, Path.Direction.CW);
+            canvas.clipPath(clipPath);
+            canvas.drawColor(Color.BLACK);
+            super.onDraw(canvas);
+        }
+    }
---
+++ app/src/main/java/com/example/skywalker/service/ChatHeadService.java
@@ -0,0 +1,54 @@
+package com.example.skywalker.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.example.skywalker.helloworld.R;
+import com.example.skywalker.rounded_image.CircularImageView;
+
+/**
+ * Created by skywalker on 2/5/15.
+ */
+public class ChatHeadService extends Service {
+
+    private WindowManager windowManager;
+    private ImageView chatHead;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        chatHead = new CircularImageView(this);
+        chatHead.setImageResource(R.drawable.ic_launcher);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager.addView(chatHead, params);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (chatHead != null) windowManager.removeView(chatHead);
+    }
+}
